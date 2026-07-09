da95eee08731694899330daeffbebf2f6da563aa
roshan <innovatewithroshan@gmail.com>
2026-07-09 12:29:05 +0530

    Add Audio Button

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,19 @@ import Navbar from "./Navbar";
 function App() {
   return (
     <>
+
+
       <div className="bg-black flex justify-center py-2">
+        
         <img
           src="https://plus.unsplash.com/premium_photo-1679826780040-c48444660e21?w=150&h=150&fit=crop&auto=format"
           alt="Car Wash Logo"
           className="h-16 object-contain"
         />
+
+
+
+
       </div>
       <div className="bg-black flex justify-center">
         <Navbar />
@@ -18,6 +25,8 @@ function App() {
 
       <Background />
       <Color />
+
+
     </>
   );
 }
